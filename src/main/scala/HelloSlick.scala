9322dfa8805107a85ceb86f21cfafe1a1afdf321
Benjamin Maurer <maurer.benjamin@gmail.com>
2024-11-08 09:57:31 +0100

    Repro

--- src/main/scala/HelloSlick.scala
+++ src/main/scala/HelloSlick.scala
@@ -62,137 +62,22 @@ object HelloSlick extends App {
 
     }.flatMap { _ =>
 
-      /* Streaming */
+      /* Reproducer for bug #1355 */
+      println("***** Reproducer for bug #1355 *****")
 
-      val coffeeNamesAction: StreamingDBIO[Seq[String], String] =
-        coffees.map(_.name).result
+      // This works
+      val workingQuery = coffees.join(suppliers).on(_.supID === _.id)
+        .groupBy(_._2.id)
+        .map { case (supID, group) => (supID, group.map(_._1.price).countDistinct) }
 
-      val coffeeNamesPublisher: DatabasePublisher[String] =
-        db.stream(coffeeNamesAction)
+      db.run(workingQuery.result).map(println)
 
-      coffeeNamesPublisher.foreach(println)
+      // This throws `slick.SlickTreeException`
+      val brokenQuery = coffees.join(suppliers).on(_.supID === _.id)
+        .groupBy(_._2.id)
+        .map { case (supID, group) => (supID, group.map(_._1.price).distinct.length) }
 
-    }.flatMap { _ =>
-
-      /* Filtering / Where */
-
-      // Construct a query where the price of Coffees is > 9.0
-      val filterQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
-        coffees.filter(_.price > 9.0)
-
-      // Print the SQL for the filter query
-      println("Generated SQL for filter query:\n" + filterQuery.result.statements)
-
-      // Execute the query and print the Seq of results
-      db.run(filterQuery.result.map(println))
-
-    }.flatMap { _ =>
-
-      /* Update */
-
-      // Construct an update query with the sales column being the one to update
-      val updateQuery: Query[Rep[Int], Int, Seq] = coffees.map(_.sales)
-
-      val updateAction: DBIO[Int] = updateQuery.update(1)
-
-      // Print the SQL for the Coffees update query
-      println("Generated SQL for Coffees update:\n" + updateQuery.updateStatement)
-
-      // Perform the update
-      db.run(updateAction.map { numUpdatedRows =>
-        println(s"Updated $numUpdatedRows rows")
-      })
-
-    }.flatMap { _ =>
-
-      /* Delete */
-
-      // Construct a delete query that deletes coffees with a price less than 8.0
-      val deleteQuery: Query[Coffees,(String, Int, Double, Int, Int), Seq] =
-        coffees.filter(_.price < 8.0)
-
-      val deleteAction = deleteQuery.delete
-
-      // Print the SQL for the Coffees delete query
-      println("Generated SQL for Coffees delete:\n" + deleteAction.statements)
-
-      // Perform the delete
-      db.run(deleteAction).map { numDeletedRows =>
-        println(s"Deleted $numDeletedRows rows")
-      }
-
-    }.flatMap { _ =>
-
-      /* Sorting / Order By */
-
-      val sortByPriceQuery: Query[Coffees, (String, Int, Double, Int, Int), Seq] =
-        coffees.sortBy(_.price)
-
-      println("Generated SQL for query sorted by price:\n" +
-        sortByPriceQuery.result.statements)
-
-      // Execute the query
-      db.run(sortByPriceQuery.result).map(println)
-
-    }.flatMap { _ =>
-
-      /* Query Composition */
-
-      val composedQuery: Query[Rep[String], String, Seq] =
-        coffees.sortBy(_.name).take(3).filter(_.price > 9.0).map(_.name)
-
-      println("Generated SQL for composed query:\n" +
-        composedQuery.result.statements)
-
-      // Execute the composed query
-      db.run(composedQuery.result).map(println)
-
-    }.flatMap { _ =>
-
-      /* Joins */
-
-      // Join the tables using the relationship defined in the Coffees table
-      val joinQuery: Query[(Rep[String], Rep[String]), (String, String), Seq] = for {
-        c <- coffees if c.price > 9.0
-        s <- c.supplier
-      } yield (c.name, s.name)
-
-      println("Generated SQL for the join query:\n" + joinQuery.result.statements)
-
-      // Print the rows which contain the coffee name and the supplier name
-      db.run(joinQuery.result).map(println)
-
-    }.flatMap { _ =>
-
-      /* Computed Values */
-
-      //#maxPrice
-      // Create a new scalar value that calculates the maximum price
-      val maxPrice: Rep[Option[Double]] = coffees.map(_.price).max
-      //#maxPrice
-
-      println("Generated SQL for max price column:\n" + maxPrice.result.statements)
-
-      // Execute the computed value query
-      db.run(maxPrice.result).map(println)
-
-    }.flatMap { _ =>
-
-      //#plainSql
-      /* Manual SQL / String Interpolation */
-
-      // A value to insert into the statement
-      val state = "CA"
-
-      // Construct a SQL statement manually with an interpolated value
-      val plainQuery = sql"select SUP_NAME from SUPPLIERS where STATE = $state".as[String]
-      //#plainSql
-
-      println("Generated SQL for plain query:\n" + plainQuery.statements)
-
-      // Execute the query
-      db.run(plainQuery).map(println)
-
+      db.run(brokenQuery.result).map(println)
     }
     Await.result(f, Duration.Inf)
 
